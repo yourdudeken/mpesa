6bad41d88f78e3a05bd0b1b3d47946fbdf155cc7
yourdudeken <kenmwendwamuthengi@gmail.com>
2026-01-25 17:46:05 +0300

    feat: Add M-Pesa test configuration files, including `.env.example` and `mpesa.php`.

---
+++ tests/config/mpesa.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * M-Pesa Project-Level Configuration
+ * 
+ * You ONLY need to provide your project credentials here.
+ * Boilerplate configuration is handled by the package.
+ */
+return [
+    // Application Environment (sandbox or live)
+    'is_sandbox'         => true,
+
+    // Credentials & Common Fields
+    'consumer_key'       => 'dGyDD1yDZ7ojyQ3xBzKGGSf6lzs9NwOJwvdOkvmq0KgHz3YD',
+    'consumer_secret'    => 'lUf3zoO0SpoEGQK0l0oquLJ6AIWo8RZWJiRZFC4IFHVP2Rv3EA733ajuXGg6C9MM',
+    'short_code'         => '174379',
+    'passkey'            => 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919',
+    'callback'           => 'https://webhook.site/e7160be4-2410-4020-8f79-139c727296b4',
+    'initiator_name'     => 'testapi',
+    'initiator_password' => 'Safaricom123!!',
+];
